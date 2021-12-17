194e220a8d0fab8f7c1501aaf7296adfb9c23d8d
CozminM <W@rdingboss00>
2021-12-17 12:59:15 +0200

    Add packages to React

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import Form from "./components/Form"
+import {Routes} from "react-router";
+import {BrowserRouter as Router, Route} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Form/>
+        <Router>
+            <Routes>
+                <Route exact path='/add-user' element={<Form/>}/>
+            </Routes>
+        </Router>
     </div>
   );
 }
