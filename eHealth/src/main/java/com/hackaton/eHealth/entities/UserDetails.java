e2a5ff0ac41894f8228de68ff3649b13ddcd305d
CozminM <W@rdingboss00>
2021-12-17 14:10:35 +0200

    Add initial components

--- eHealth/src/main/java/com/hackaton/eHealth/entities/UserDetails.java
+++ eHealth/src/main/java/com/hackaton/eHealth/entities/UserDetails.java
@@ -1,30 +1,34 @@
-//package com.hackaton.eHealth.entities;
-//
-//import lombok.AllArgsConstructor;
-//import lombok.Getter;
-//import lombok.NoArgsConstructor;
-//import lombok.Setter;
-//
-//import javax.persistence.*;
-//import java.util.HashMap;
-//import java.util.Map;
-//
-//@Entity
-//@Getter
-//@Setter
-//@NoArgsConstructor
-//@AllArgsConstructor
-//public class UserDetails {
-//
-//    @Id
-//    @Column(name = "id", nullable = false)
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private int id;
-//
-//    @Column(name="intake", nullable = false)
-//    private Map<String, String> intakeCalories;
-//
-//    @Column(name="consumed", nullable = false)
-//    private Map<String, Integer> consumedCalories;
-//
-//}
+package com.hackaton.eHealth.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDetails {
+
+    @Id
+    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "date", nullable = false)
+    private LocalDateTime date;
+
+    @Column(name="intake", nullable = false)
+    private int intakeCalories;
+
+    @Column(name="consumed", nullable = false)
+    private int consumedCalories;
+
+}
